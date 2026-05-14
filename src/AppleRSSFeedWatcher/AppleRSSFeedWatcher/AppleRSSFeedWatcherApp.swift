73400494018f085d0c2b43ed735412a7a1c001d0
Karin Berg <karin@karinberg.de>
2026-05-14 15:27:03 +0200

    Add new Swift project

---
+++ src/AppleRSSFeedWatcher/AppleRSSFeedWatcher/AppleRSSFeedWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleRSSFeedWatcherApp.swift
+//  AppleRSSFeedWatcher
+//
+//  Created by Karin Berg on 14.05.26.
+//
+
+import SwiftUI
+
+@main
+struct AppleRSSFeedWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ src/AppleRSSFeedWatcher/AppleRSSFeedWatcherTests/AppleRSSFeedWatcherTests.swift
@@ -0,0 +1,19 @@
+//
+//  AppleRSSFeedWatcherTests.swift
+//  AppleRSSFeedWatcherTests
+//
+//  Created by Karin Berg on 14.05.26.
+//
+
+import Testing
+@testable import AppleRSSFeedWatcher
+
+struct AppleRSSFeedWatcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
